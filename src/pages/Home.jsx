dfc6e16e8a47e884853205d0e5747635866c46f0
MarkStratford <markhstratford@gmail.com>
2025-04-07 15:01:28 -0700

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// src/pages/Home.jsx
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-4xl font-bold">Hey, I'm Mark 👋</h1>
+      <p className="mt-4 text-lg">
+        Jack-of-all-trades. Dad. F1 fan. Challenge guy. Currently mastering
+        the art of not stepping on LEGO.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
